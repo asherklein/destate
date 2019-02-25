d953946f313d007a11fd77465b5839b04ff9240b
Asher <aklein@descriptsw.com>
2019-02-25 16:57:52 -0500

    Init

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const actions = []
+const { __, compose: c, mapObjectIndexed: mapo, keys, eqProps } = require('ramda')
+
+const transactions = []
+
+// address = {prop: 1, prop2: 2}
+const send = (address, content) => transactions.push({ address: address || {}, content: content || {} })
+
+const isAddressedToMe = myAddress => trans => c(all(eqProps(__, myAddress, trans.address)), keys)(trans.address)
+
+// const latest = (_, { content }) => content 
+
+// export const NEW_STATE = '@@__NEW_STATE__@@'
+
+const transReducer = (reducers) => (acc, content) => mapo((r, k) => r(acc[k], content), reducers)
+
+const getState = (myAddress, reducers) => c(reduce(transReducer(reducers)), filter(isAddressedToMe(myAddress)))(transactions)
+
+module.exports = { getState, send }
